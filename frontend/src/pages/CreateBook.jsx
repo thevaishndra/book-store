bbd79ca4d1691b46b1cf78b61a4aacc145330020
thevaishndra <chandrav306m@gmail.com>
2025-02-07 01:49:44 +0530

    deployment and debugging

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -25,7 +25,7 @@ const CreateBook = () => {
     };
     setLoading(true);
     axios
-      .post("${apiUrl}/books", data)
+      .post(`${apiUrl}/books`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Book created successfully", { variant: "success" });
